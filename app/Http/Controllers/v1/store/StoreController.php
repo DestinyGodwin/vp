9294eec6390e93e50074ceca26a2a36a29616702
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-04-26 11:43:27 +0100

    added storecontroller

---
+++ app/Http/Controllers/v1/store/StoreController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\v1\store;
+
+use App\Models\Store;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\v1\store\CreateStoreRequest;
+
+class StoreController extends Controller
+{
+    public function index()
+    {
+        return Store::where('user_id', auth()->id())->get();
+    }
+
+    public function store(CreateStoreRequest $request)
+    {
+        $geo = geocodeAddress($request->address);
+
+        $store = auth()->user()->stores()->create([
+            ...$request->validated(),
+            'latitude' => $geo['lat'],
+            'longitude' => $geo['lng'],
+            'next_payment_due' => now()->addDays(30),
+        ]);
+
+        return response()->json(['message' => 'Store created.', 'store' => $store]);
+    }
+
+    public function show(Store $store)
+    {
+        $this->authorize('view', $store);
+        return $store;
+    }
+
+    public function update(CreateStoreRequest $request, Store $store)
+    {
+        $this->authorize('update', $store);
+
+        $geo = geocodeAddress($request->address);
+        $store->update([
+            ...$request->validated(),
+            'latitude' => $geo['lat'],
+            'longitude' => $geo['lng'],
+        ]);
+
+        return response()->json(['message' => 'Store updated.', 'store' => $store]);
+    }
+
+    public function destroy(Store $store)
+    {
+        $this->authorize('delete', $store);
+        $store->delete();
+        return response()->json(['message' => 'Store deleted.']);
+    }
+}
