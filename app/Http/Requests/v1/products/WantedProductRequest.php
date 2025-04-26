610b671aacaedcaad41f79863f3ae33c29708975
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-04-26 20:58:50 +0100

    added frm request to store wanted products

---
+++ app/Http/Requests/v1/products/WantedProductRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\v1\products;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class WantedProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'description' => ['nullable', 'string'],
+            'category_id' => ['required', 'exists:categories,id'],
+            'images' => ['nullable', 'array'],
+            'images.*' => ['image', 'mimes:jpeg,png,jpg', 'max:2048'], // each file must be an image
+        ];
+    }
+}
