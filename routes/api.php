1d76742439fedc4dea9bc71d28e22ea3fbf44ff0
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-04-26 20:40:25 +0100

    added university seeder

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\v1\StoreController;
 use App\Http\Controllers\v1\ProductController;
+use App\Http\Controllers\v1\CategoryController;
 use App\Http\Controllers\v1\UniversityController;
 
 Route::get('/user', function (Request $request) {
@@ -12,4 +13,6 @@ Route::get('/user', function (Request $request) {
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('universities', UniversityController::class);
     Route::apiResource('stores', StoreController::class);
-    Route::apiResource('products', ProductController::class);});
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('products', ProductController::class);}
+);
