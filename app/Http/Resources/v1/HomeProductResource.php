53d135e45f746d28eeabfae198b79088b412158f
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-04-28 21:02:43 +0100

    added homeproduct esource

---
+++ app/Http/Resources/v1/HomeProductResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\v1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class HomeProductResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'store' => [
+                'id' => $this->store->id,
+                'name' => $this->store->name,
+                'university' => $this->store->university->name ?? null,
+            ],
+            'name' => $this->name,
+            'description' => $this->description,
+            'price' => $this->price,
+            'category' => $this->category->name ?? null,
+            'images' => $this->images->pluck('url'), 
+            'created_at' => $this->created_at->toDateTimeString(),
+        ];
+    }
+}
